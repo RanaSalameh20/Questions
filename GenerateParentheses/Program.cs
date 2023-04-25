c8c4f6b79131a9631919597d86e0182eb105785b
RanaSalameh <rana.salameh313@gmail.com>
2023-04-25 17:04:13 +0300

    Refactor the code and add .gitignore file

--- GenerateParentheses/Program.cs
+++ GenerateParentheses/Program.cs
@@ -4,46 +4,20 @@ using System.Collections.Generic;
 
 namespace GenerateParentheses
 {
-
-
-    public class ParenthesesGenerator
+    public class Program
     {
         public static void Main(string[] args)
         {
             //tese Case 1 
-            int n = 3;
-            var result = AllParenthesis(n);
-            Console.WriteLine($"All valid combinations of {n} pairs of parentheses:");
-            foreach (string s in result)
+            ParenthesesGenerator paranthesesGenerator = new ParenthesesGenerator();
+            int numberOfParantheses = 3;
+            var combinations = paranthesesGenerator.CreateAllParenthesis(numberOfParantheses);
+            Console.WriteLine($"All valid combinations of {numberOfParantheses} pairs of parentheses:");
+            foreach (var combination in combinations)
             {
-                Console.WriteLine(s);
+                Console.WriteLine(combination);
             }
         }
-        private static void Generate(int open, int close, List<string> ans, string str)
-        {
-            // (3,3,{} , "")
-            if (open == 0 && close == 0)
-            {
-                ans.Add(str);
-                return;
-            }
-            if (open != 0)
-            {
-                Generate(open - 1, close, ans, str + "(");
-            }
-            if (close > open)
-            {
-                Generate(open, close - 1, ans, str + ")");
-            }
-        }
-
-        public static List<string> AllParenthesis(int n) //1
-        {
-            List<string> ans = new List<string>(); // { } 
-            Generate(n, n, ans, ""); // (1,1,{} , "")
-            return ans;  // 
-        }
+     
     }
-
-
 }
